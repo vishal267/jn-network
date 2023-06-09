6fe90f17814dc34a0088309c720d26545e9e9cb6
vishal267 <vishal267@ymail.com>
2023-06-10 02:56:42 +0530

    Create variables.tf

---
+++ terraform/resourece/s3_backend/variables.tf
@@ -0,0 +1,16 @@
+variable "region_name" {
+  description = "Region name to launch VPC network"
+  type        = string
+  default     = "us-east-1"
+}
+variable "bucket_name" {
+  description = "bucket name for terraform state files"
+  type        = string
+  default     = "k8s-webapp-tf"
+}
+
+variable "environment" {
+  description = "Environment name for deployment"
+  type        = string
+  default     = "production"
+}
