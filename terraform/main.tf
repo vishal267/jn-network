ca6213623d585e96bdb3e9be33b1e194a481a4a1
vishal267 <vishal267@ymail.com>
2023-06-10 02:24:02 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+module "vpc" {
+  source  = "registry.terraform.io/terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = local.cluster_name
+  cidr = "172.25.126.0/23"
+
+
+  private_subnets = ["172.25.126.0/26", "172.25.126.64/26"]
+  public_subnets  = ["172.25.126.128/26", "172.25.126.192/26"]
+
+  azs = ["us-east-1a", "us-east-1b"]
+
+
+
+  map_public_ip_on_launch = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  one_nat_gateway_per_az  = false
+
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # https://aws.amazon.com/premiumsupport/knowledge-center/eks-vpc-subnet-discovery/
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}"  = "owned"
+    "karpenter.sh/discovery/${local.cluster_name}" = local.cluster_name
+    "kubernetes.io/role/internal-elb"              = "1"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+}
