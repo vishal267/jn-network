c952975e2ddf930fd5b0e51f8656b7b648650222
vishal267 <vishal267@ymail.com>
2023-06-11 03:37:43 +0530

    Update outputs.tf

--- terraform/resources/vpc/outputs.tf
+++ terraform/resources/vpc/outputs.tf
@@ -1,3 +1,11 @@
-output "vpc" {
-  value = module.vpc
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc_owner_id" {
+  value = module.vpc.vpc_owner_id
 }
